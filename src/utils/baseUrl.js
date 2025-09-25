c1c7bef4531ecc759bb4d9d9a5c6d79860ea5f38
seunfola <seunfola1@gmail.com>
2025-09-25 02:49:31 +0100

    fixing the base url issue and commiting proprly

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,11 +1,9 @@
 let base_url;
+
 if (import.meta.env.MODE === "development") {
   base_url = "http://localhost:8080/api/";
 } else {
-  base_url = "https://grocery-backend-nkxh.onrender.com";
+  base_url = "https://grocery-backend-nkxh.onrender.com/api/";
 }
 
 export { base_url };
-
-
-
