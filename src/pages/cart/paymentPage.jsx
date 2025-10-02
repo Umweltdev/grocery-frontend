2ff0b35b5f8e6dba6aff19b61cce545b93460319
Ridwan Abdulsalam <55768183+Sirlahm@users.noreply.github.com>
2025-10-02 18:16:01 +0100

    Update paymentPage.jsx

--- src/pages/cart/paymentPage.jsx
+++ src/pages/cart/paymentPage.jsx
@@ -82,7 +82,7 @@ const PaymentPage = ({ defaultDeliveryDate }) => {
         },
         { headers: { Authorization: `Bearer ${user?.token}` } }
       );
-      console.log({pk: import.meta.env. VITE_STRIPE_PUBLISHABLE_KEY})
+      
       if (option === "card" && response.data.sessionId) {
         const stripe = await loadStripe(
           import.meta.env. VITE_STRIPE_PUBLISHABLE_KEY
