fe2834c8adaf613bbc022d0e03cb927644a9e098
seunfola <seunfola1@gmail.com>
2025-10-02 03:58:22 +0100

    added the docker configuration and fixes

--- src/pages/admin-dashboard/data.js
+++ src/pages/admin-dashboard/data.js
@@ -1,28 +1,25 @@
-import { ReactComponent as Pie } from "../../assets/icons/piechart.svg";
+import Pie from "../../assets/icons/piechart.svg?component";
 
 export const mockData = [
-    {
-      name: "Order",
-      amount: "32,350",
-      amount1: "9345",
-      percentage: "25.25%",
-      color: "#4E97FD",
-    },
-    {
-      name: "Sold Sales",
-      amount: "2,350",
-      amount1: "9345",
-      percentage: "2.25%",
-      color: "#E94560",
+  {
+    name: "Order",
+    amount: "32,350",
+    amount1: "9345",
+    percentage: "25.25%",
+    color: "#4E97FD",
   },
-        {
-      name: "Product Share",
-      amount: "£2,350",
-      Icon: Pie,
-      percentage: "2.25%",
-      color: "green",
-    },
-  ];
-  
-
-  
+  {
+    name: "Sold Sales",
+    amount: "2,350",
+    amount1: "9345",
+    percentage: "2.25%",
+    color: "#E94560",
+  },
+  {
+    name: "Product Share",
+    amount: "£2,350",
+    Icon: Pie,
+    percentage: "2.25%",
+    color: "green",
+  },
+];
