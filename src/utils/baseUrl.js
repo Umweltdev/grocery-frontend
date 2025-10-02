8483f29a5b215f33dfc92871fcad371919ba52ed
seunfola <seunfola1@gmail.com>
2025-10-02 04:14:56 +0100

    fixed the base url glitch

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,3 +1,9 @@
-const base_url = import.meta.env.VITE_API_BASE_URL + "/api/";
+let base_url;
+
+if (import.meta.env.MODE === "development") {
+  base_url = "http://localhost:8080/api/";
+} else {
+  base_url = "https://grocery-backend-nkxh.onrender.com/api/";
+}
 
 export { base_url };
