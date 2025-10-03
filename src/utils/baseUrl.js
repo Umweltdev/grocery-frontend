be4f74ae71deeb40e8189c612952892a98496013
seunfola <seunfola1@gmail.com>
2025-10-03 02:16:37 +0100

    Your commit message here

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,7 +1,7 @@
 let base_url;
 
 if (import.meta.env.MODE === "development") {
-  base_url = "http://localhost:8080/api/";
+  base_url = "http://72.61.17.35:8080/api/";
 } else {
   base_url = "https://grocery-backend-nkxh.onrender.com/api/";
 }
