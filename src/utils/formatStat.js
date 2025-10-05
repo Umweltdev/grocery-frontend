249e0b0d5c76afac8ec8a60e1230b8291599ad6f
seunfola <seunfola1@gmail.com>
2025-10-05 23:37:41 +0100

    refactor the formatStat as a reusable piece

--- src/utils/formatStat.js
+++ src/utils/formatStat.js
@@ -1,18 +1,19 @@
+// Instantiate formatters once for better performance
+const currencyFormat = new Intl.NumberFormat("en-GB", {
+  style: "currency",
+  currency: "GBP",
+  minimumFractionDigits: 2,
+});
 
-export const currencyFormatter = (value) =>
-  new Intl.NumberFormat("en-GB", {
-    style: "currency",
-    currency: "GBP",
-    minimumFractionDigits: 2,
-  }).format(value);
+const percentFormat = new Intl.NumberFormat("en-GB", {
+  style: "percent",
+  minimumFractionDigits: 2,
+});
 
-export const percentFormatter = (value) =>
-  new Intl.NumberFormat("en-GB", {
-    style: "percent",
-    minimumFractionDigits: 2,
-  }).format(value / 100);
+const quantityFormat = new Intl.NumberFormat("en-GB", {
+  maximumFractionDigits: 0,
+});
 
-export const quantityFormatter = (value) =>
-  new Intl.NumberFormat("en-GB", {
-    maximumFractionDigits: 0,
-  }).format(value);
+export const currencyFormatter = (value) => currencyFormat.format(value);
+export const percentFormatter = (value) => percentFormat.format(value / 100);
+export const quantityFormatter = (value) => quantityFormat.format(value);
